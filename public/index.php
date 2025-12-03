637dcd798c06fe1dd8a734f4c45f9b4e6d110193
Dmitri Demenkov <dmitridemenkov@gmail.com>
2025-12-03 21:21:07 +0300

    Add Tailwind CSS configuration and files

--- public/index.php
+++ public/index.php
@@ -36,7 +36,7 @@ $currentDate = date(Config::DATE_FORMAT);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>🍽️ Трекер Питания</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/output.css">
 </head>
 <body>
     <div class="container">
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.{html,php,js}",
+    "./src/**/*.php",
+    "./*.php"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
